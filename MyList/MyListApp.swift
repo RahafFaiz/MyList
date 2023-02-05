ed49bad0b15b29c79803ed9952d13a40f8b9b1dc
R <raghad.fayez1@gmail.com>
2023-02-06 00:48:01 +0300

    Initial Commit

---
+++ MyList/MyListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyListApp.swift
+//  MyList
+//
+//  Created by Raghad Fayez on 15/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MyListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
